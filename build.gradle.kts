351133c75bf2946704b2b8d2351bafb0f8db22bd
Kaddicus <47439649+KadTheHunter@users.noreply.github.com>
2026-09-10 14:57:05 -0400

    update: ver 1.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("java")
     id("java-library")
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"  // Check for newer
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.23"  // Check for newer
     id("xyz.jpenilla.run-paper") version "2.3.1"  // For easy testing
 }
 
 group = "org.kaddicus"
-version = "1.4.0"
+version = "1.4.1"
 description = "Protects Item Frames, Paintings, and other Hanging Entities from being destroyed by Projectiles, and provides a way to remove Armor Stands with negative Health / DeathTime values."
 
 repositories {
